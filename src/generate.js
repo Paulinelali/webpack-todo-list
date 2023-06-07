32a70d001dbc44cd98400d5c31721673c44e1073
Ayomide <ayomidesunday31@gmail.com>
2023-06-08 00:05:19 +0100

    Set up webpack work env

---
+++ src/generate.js
@@ -0,0 +1,3 @@
+export const joke = () => {
+    console.log("Jokes")
+}
---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+
+import { joke } from "./generate.js";
+import './styles/main.scss'
+
+console.log("working")
+console.log(joke())
+console.log(90)
